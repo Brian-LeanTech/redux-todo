ce80d9f17a30b23cd1cd8054de2b26cc74429a27
Brian-LeanTech <bbernal@lean-tech.io>
2020-01-19 23:25:16 -0500

    separando el reducer y construyendo el reducer principal sin el api de redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -5,43 +5,54 @@ import {
     COMPLETE_TODO
 } from './actions';
 
-const initialState = {
-    visibilityFilter: VisibilityFilters.SHOW_ALL,
-    todos: []
-};
-
-function todoApp(state = initialState, action) {
+function todos(state = [], action) {
     switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            // Aqui tambien se puede usar spread operator
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            });
-
         case ADD_TODO:
-            return Object.assign({}, state, {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.types,
-                        completed: false
-                    }
-                ]
-            });
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ];
 
         case COMPLETE_TODO:
-            return Object.assign({}, state, {
-                todos: state.todos.map((todo, index) => {
-                    if (index === action.index) {
-                        return Object.assign({}, todo, {
-                            completed: true
-                        });
-                    }
-                    return todo;
-                })
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: true
+                    });
+                }
+                return todo;
             });
 
         default:
             return state;
     }
+}
+
+function visibilityFilter(state = 'SHOW_ALL', action) {
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter;
+
+        default:
+            return state;
+    }
+}
+
+/**
+ * Ahora el reducer principal simplemente llama a
+ * los demas sub-reducers que controlan distintas partes
+ * del state, y los combina en un objeto.
+ *
+ * Los valores iniciales del state son determinados
+ * por los default de cada uno de los sub-reducers
+ * (ver nota completa en el cuaderno .. separando reducers)
+ */
+function todoApp(state = initialState, action) {
+    return {
+        visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+        todos: todos(state.todos, action)
+    };
 }
