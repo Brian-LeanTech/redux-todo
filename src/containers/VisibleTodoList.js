2b363296710c4092edd3f79db474767bb1a05648
Brian-LeanTech <bbernal@lean-tech.io>
2020-01-22 11:21:11 -0500

    refactor from english docs. It is much better

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,34 +1,32 @@
 import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import TodoList from '../components/TodoList';
+import { VisibilityFilters } from '../actions'
 
 const getVisibleTodos = (todos, filter) => {
     switch (filter) {
-        case 'SHOW_ALL':
+        case VisibilityFilters.SHOW_ALL:
             return todos;
-        case 'SHOW_COMPLETED':
+        case VisibilityFilters.SHOW_COMPLETED:
             return todos.filter(t => t.completed);
-        case 'SHOW_ACTIVE':
+        case VisibilityFilters.SHOW_ACTIVE:
             return todos.filter(t => !t.completed);
         default:
-            throw Error('error in getVisibleTodos... Esto nunca va a pasar');
+            throw Error('error in getVisibleTodos... Esto debería pasar: ' + filter);
     }
 };
 
-const mapStateToProps = state => {
-    return {
-        todos: getVisibleTodos(state.todos, state.visibilityFilter)
-    };
-};
+const mapStateToProps = state => ({
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+})
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onTodoClick: id => {
-            dispatch(toggleTodo(id));
-        }
-    };
-};
+const mapDispatchToProps = dispatch => ({
+    toggleTodo: id => dispatch(toggleTodo(id))
+})
 
-const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
+const VisibleTodoList = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList)
 
 export default VisibleTodoList;
